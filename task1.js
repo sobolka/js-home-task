c795e0f1d6a7e25abdeaf4455d89cfa5be2386ae
olya.zelenak <volha.zelianiak@alseda.by>
2021-05-04 19:11:23 +0300

    homework on data types

---
+++ task1.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+let radius = 12;
+let circleLength = 2 * radius* PI;
+console.log(circleLength);
+circleLength = 2 * radius* Math.PI;
+console.log(circleLength);
+
+let MINUTES_IN_HOUR = 60;
+let SECONDS_IN_MINUTE = 60;
+let secondsInHour = MINUTES_IN_HOUR * SECONDS_IN_MINUTE;
+console.log(secondsInHour);
---
+++ task2.js
@@ -0,0 +1,26 @@
+let num = 47;
+num = num + 7;
+num = num - 18;
+num = num * 10;
+num = num / 15;
+alert(num);
+
+num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+let numVar = 10;
+numVar = numVar + 1;
+numVar = numVar + 1;
+numVar = numVar - 1;
+alert(numVar);
+
+numVar = 10;
+numVar++;
+++numVar;
+numVar--;
+alert(numVar);
+
